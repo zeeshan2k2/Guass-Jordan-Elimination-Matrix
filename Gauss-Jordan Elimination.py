fe1b4475be1cf2344e11852c46c0d2287c794a78
Zeeshan <100070393+zeeshan2k2@users.noreply.github.com>
2022-08-25 03:06:01 +0500

    Add files via upload

--- Gauss-Jordan Elimination.py
+++ Gauss-Jordan Elimination.py
@@ -3,7 +3,8 @@
 def printmatrix():
     for i in matrix:
         for j in i:
-            # if you want more or less numbers after decimal point the adjust %.nf accordingly where n is the number of units
+            # if you want more or less numbers after decimal point to adjust %.nf accordingly where n is the number
+            # of units
             print("%.1f" % j, end="  ")
         print()
 
@@ -30,6 +31,7 @@ def pivotelement():
             for col in range(len(matrix)):
                 matrix[row][col] = matrix[row][col] - make_zero * matrix[rows][col]
 
+
 # to solve the pivot column and remove negative sign from all zeros.
 def solve_pivotelement():
     print()
@@ -43,22 +45,35 @@ def solve_pivotelement():
                 continue
     printmatrix()
     print()
-    print("This is the final Matrix")
 
+
+# this function will be used to solve the number of time we'll have to solve pivot column
+def no_of_pivot_element():
+    print()
+    printmatrix()
+    print()
+    for i in range(row_number):
+        number = ["first", "second", "third", "fourth", "fifth", "sixth"]
+        n = i
+        print("For", number[n], "pivot element enter the values of row and column")
+        solve_pivotelement()
+
+# taking input for rows and column
 print("What type of matrix do you want to solve")
 row_number = int(input("rows : "))
 print(row_number)
 column_number = int(input("columns : "))
 print(column_number)
+# setting some pre-conditions
 if column_number != row_number:
-    print("Sorry! cannot solve this matric the number of rows and column must be equal and this program supports "
-          "matrices (2*2 to 5*5).")
+    print("Sorry! cannot solve this matrix the number of rows and column must be equal. This program only supports "
+          "matrices (2*2 to 6*6).")
     quit()
-elif column_number > 5 and row_number > 5:
-    print("Sorry! cannot solve this matric the number of rows and column must be equal and this program supports "
-          "matrices (2*2 to 5*5).")
+elif column_number > 6 and row_number > 6:
+    print("Sorry! cannot solve this matrix the number of rows and column must be equal. This program only supports "
+          "matrices (2*2 to 6*6).")
     quit()
-elif column_number == 1 and row_number == 1:
+elif column_number <= 1 and row_number <= 1:
     print("This matrix is not possible")
     quit()
 
@@ -66,87 +81,29 @@ n = 0
 # taking input for matrix
 matrix = [[0 for i in range(column_number)] for x in range(row_number)]
 while n != row_number:
-    print("Enter numbers for row")
-    for i in range (column_number):
+    number = ["first", "second", "third", "fourth", "fifth", "sixth"]
+    print()
+    print("Enter numbers for", number[n], "row")
+    for i in range(column_number):
         data = int(input())
-        matrix[n][i]=data
-    n = n+1
+        matrix[n][i] = data
+    n = n + 1
 # This matrix is not solvable i.e gives an error
 matrix_error = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 if matrix == matrix_error:
     print("This matrix is not possible")
-elif column_number == 2 and row_number == 2:
-    print()
-    printmatrix()
-    print()
-    # for first pivot element
-    print("For first pivot element enter the values")
-    solve_pivotelement()
 
-    # for second pivot element
-    print("For second pivot element enter the values")
-    solve_pivotelement()
+elif column_number == 2 and row_number == 2:
+    no_of_pivot_element()
 
 elif column_number == 3 and row_number == 3:
-    print()
-    printmatrix()
-    print()
-    # for first pivot element
-    print("For first pivot element enter the values")
-    solve_pivotelement()
-
-    # for second pivot element
-    print("For second pivot element enter the values")
-    solve_pivotelement()
-
-    # for third pivot element
-    print("For third pivot element enter the values")
-    solve_pivotelement()
+    no_of_pivot_element()
 
 elif column_number == 4 and row_number == 4:
-    print()
-    printmatrix()
-    print()
-    # for first pivot element
-    print("For first pivot element enter the values")
-    solve_pivotelement()
-
-    # for second pivot element
-    print("For second pivot element enter the values")
-    solve_pivotelement()
-
-    # for third pivot element
-    print("For third pivot element enter the values")
-    solve_pivotelement()
-
-    # for fourth pivot element
-    print("For fourth pivot element enter the values")
-    solve_pivotelement()
+    no_of_pivot_element()
 
 elif column_number == 5 and row_number == 5:
-    print()
-    printmatrix()
-    print()
-    # for first pivot element
-    print("For first pivot element enter the values")
-    solve_pivotelement()
+    no_of_pivot_element()
 
-    # for second pivot element
-    print("For second pivot element enter the values")
-    solve_pivotelement()
-
-    # for third pivot element
-    print("For third pivot element enter the values")
-    solve_pivotelement()
-
-    # for fourth pivot element
-    print("For fourth pivot element enter the values")
-    solve_pivotelement()
-
-    # for fifth pivot element
-    print("For fifth pivot element enter the values")
-    solve_pivotelement()
-
-
-
-
+elif column_number == 6 and row_number == 6:
+    no_of_pivot_element()
